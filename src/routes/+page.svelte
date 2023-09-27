fa59ef1171f0c230601ec0a08e8ebe3461367ffa
ivan wolyniec <ivanwolyniec@gmail.com>
2023-09-27 11:20:23 -0400

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to Dateroit</h1>
+<h2>main</h2>
+<p>testing cloudflare redirect rules: visit <a href="https://developers.cloudflare.com/rules/url-forwarding/single-redirects/create-dashboard/">https://developers.cloudflare.com/rules/url-forwarding/single-redirects/create-dashboard/</a> to read the documentation</p>
